76cb8cec1f22f8e951253d85ad3c6d9ad55fb39b
rae-droid <chriscater523@gmail.com>
2025-04-07 10:01:51 +0000

    movie updates

---
+++ app.py
@@ -0,0 +1,98 @@
+# print("Ramsey")
+# print('o----')
+# print(' ||||')
+# print('*' * 10)
+from test.test_tomllib.burntsushi import convert
+
+# PRICING
+# price = 10
+# rating = 4.9
+# name = 'Ramsey'
+# is_published = True
+# print(price)
+
+# PATIENT
+# full_name = 'John Smith'
+# age = 20
+# is_new = True
+
+# name = input('What is your name? ')
+# favorite_color = input('What is your favorite color? ')
+# print(name + ' likes ' + favorite_color)
+# int()  ..converting a string into an integer
+# float() ...converting a string into a float/decimals
+# bool()  ...converting a string into a boolian valur
+
+
+# DATE OF BIRTH
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2025 - int(birth_year)
+print(type(age))
+print(age)
+
+
+# CHeCKING WEIGHT
+# weight_lbs = input('weight (lbs): ')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+
+
+# course = 'Python for Beginners'
+# another = course[:]
+# print(another)
+
+
+# name = 'Jennifer'
+# print(name[1:-1])
+
+# first = 'John'
+# last = 'Smith'
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first}  [{last}] is a coder'
+# print(msg)
+
+
+
+# course = 'Python for Beginners'
+# print(len(course))
+# print(course.upper())
+# print(course)
+# print(course.lower())
+# print(course.find('Beginners'))
+# print(course.replace('P', 'J'))
+# print('python' in course)
+# len()
+# course.upper()converting string into upper case
+# course.lower()converting string into lower case
+# cousre.find()convert
+
+
+# print(10 // 3)
+# print(10 + 3)
+# print(10 - 3)
+# print(10 / 3)
+# print(10 % 3)
+# print(10 ** 3)
+
+# x = 10
+# # x = x + 3
+# x -= 3
+# print(x)
+
+# x = (10 + 3) * 2 ** 2
+# print(x)
+# exponentiation 2 ** 3
+# multiplication or divisio 
+# addition or subtraction
+# parenthesis
+# x = (2 + 3) * 10 - 3
+# print(x)
+
+# import math
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9))
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
---
+++ myjs/sidebar.js
@@ -0,0 +1,45 @@
+function openSidebar() {
+  document.getElementById("sidebar").style.width = "180px";
+  document.getElementById("openbtn").style.display = "none"
+  
+  
+}
+
+function closeSidebar() {
+  document.getElementById("openbtn").style.display = "block"
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  //  document.getElementById("openbtn").style.display = ""
+  // document.getElementById("openbtn").style.display = "block"
+}
+function search() {
+  // document.getElementById("search").style.width = "300px";
+  // document.getElementById("search").style.transition = "width 8s ease-in-out";
+//  document.getElementById("search").style.display = "none"
+}
+
+// function login() {
+//   document.getElementsByClassName("container")[0].style.display = ""
+// }
+
+
+
+
+
+// function main(openSidebar) {
+//   document.getElementById("main").style.opacity = "0.3"; 
+
+  
+// }
+
+// function sidebar() {
+//   document.getElementById("sidebarone").style.width = "auto"
+//   document.getElementById("sidebarone").style.height = "auto"
+// }
+
+// if(openbtn){
+//   document.getElementById("openSidebar").style.opacity = "0"
+
+// } else{
+//   document.getElementById("")
+// }
