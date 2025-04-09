b0d040b82fbbcf97d483ad70e7df973e130327e0
rae-droid <chriscater523@gmail.com>
2025-04-09 23:59:23 +0000

    new

--- myjs/sidebar.js
+++ myjs/sidebar.js
@@ -1,45 +1,19 @@
 function openSidebar() {
-  document.getElementById("sidebar").style.width = "180px";
-  document.getElementById("openbtn").style.display = "none"
-  
-  
+  document.getElementById("sidebar").style.width = "250px"; 
+  document.getElementById("openbtn").style.display = "none"; 
 }
 
 function closeSidebar() {
-  document.getElementById("openbtn").style.display = "block"
-  document.getElementById("sidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-  //  document.getElementById("openbtn").style.display = ""
-  // document.getElementById("openbtn").style.display = "block"
-}
-function search() {
-  // document.getElementById("search").style.width = "300px";
-  // document.getElementById("search").style.transition = "width 8s ease-in-out";
-//  document.getElementById("search").style.display = "none"
+  document.getElementById("sidebar").style.width = "0"; 
+  document.getElementById("openbtn").style.display = "block";
 }
 
-// function login() {
-//   document.getElementsByClassName("container")[0].style.display = ""
-// }
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
 
-
-
-
-
-// function main(openSidebar) {
-//   document.getElementById("main").style.opacity = "0.3"; 
-
-  
-// }
-
-// function sidebar() {
-//   document.getElementById("sidebarone").style.width = "auto"
-//   document.getElementById("sidebarone").style.height = "auto"
-// }
-
-// if(openbtn){
-//   document.getElementById("openSidebar").style.opacity = "0"
-
-// } else{
-//   document.getElementById("")
-// }
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
